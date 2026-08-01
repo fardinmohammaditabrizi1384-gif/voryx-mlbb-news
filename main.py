2f506ef8d1dfea479ce8ab5584c458ff3f27886a
fardinmohammaditabrizi1384-gif <fardinmohammaditabrizi1384@gmail.com>
2026-08-01 12:55:59 +0330

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import os
+import requests
 
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+data = {
+    "chat_id": CHAT_ID,
+    "text": "✅ اتصال Voryx News Bot با موفقیت انجام شد!"
+}
+
+response = requests.post(url, data=data)
+
+print(response.json())
